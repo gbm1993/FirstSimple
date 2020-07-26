fe8aea35dfd623e3526ca1a79a1bdb4e41dfdfbc
gbm1993 <68726259+gbm1993@users.noreply.github.com>
2020-07-26 11:56:12 +0530

    my first commit

---
+++ src/test/java/com/example/simplehellowwithec2/Simplehellowwithec2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplehellowwithec2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Simplehellowwithec2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
